a9da45d291a69b7e02dc3daec8d83b6129e59f98
Anamol <anmoldkl971@gmail.com>
2023-12-05 13:18:13 +0545

    version 1.0

--- dataRetriever.py
+++ dataRetriever.py
@@ -2,6 +2,6 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-url = ""
+url = "https://www.nasdaq.com/market-activity/stocks/aapl/historical"
 r = requests.get(url)
 print(r)
