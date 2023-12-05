cdbe69c806f0a0bcec48658673f05d8b8070884d
Anamol <anmoldkl971@gmail.com>
2023-12-05 13:19:06 +0545

    version 1.0

--- dataRetriever.py
+++ dataRetriever.py
@@ -3,5 +3,10 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 url = "https://www.nasdaq.com/market-activity/stocks/aapl/historical"
-r = requests.get(url)
-print(r)
+response = requests.get(url)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
