f888889c3d8609ebb680412feb56521a71373b33
AnamolZ <anmoldkl971@gmail.com>
2023-12-06 23:21:18 +0545

    version 1.0

--- dataRetriever.py
+++ dataRetriever.py
@@ -19,12 +19,19 @@ for header_tag in headers_tag:
     column_headers.append(header_tag.text)
 
 df = pd.DataFrame(columns=column_headers)
-data_rows = table_tag.find_all("tr")[1:11]
+data_rows = table_tag.find_all("tr")[1:101]
 
 for row_tag in data_rows:
     data_cells = row_tag.find_all("td")
     row_data = [cell.text for cell in data_cells]
     df_length = len(df)
     df.loc[df_length] = row_data
+
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+df["Volume"] = pd.to_numeric(df["Volume"].str.replace(",", ""), errors="coerce").astype('Int64')
+
+df[column_headers[1:]] = df[column_headers[1:]].apply(pd.to_numeric, errors='coerce')
+df.sort_index(inplace=True)
 
 print(df)
