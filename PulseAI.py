ad5f50618927df6f2ec787c2626fcacbffbfa932
AnamolZ <anmoldkl971@gmail.com>
2023-12-07 00:28:02 +0545

    version 1.0

--- PulseAI.py
+++ PulseAI.py
@@ -2,10 +2,14 @@ from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import pandas as pd
+import os
+from dataRetriever import func
 
 loaded_model = load_model("LSTMmodel")
 
-new_data_df = pd.read_csv("GOOG.csv", index_col=0)
+initial_price = func()
+TrainningData = "TrainningData.csv" 
+new_data_df = pd.read_csv("TrainningData.csv", index_col=0)
 new_data_df['date'] = pd.to_datetime(new_data_df.index)
 
 min_max_scaler = MinMaxScaler(feature_range=(0, 1))
@@ -19,5 +23,15 @@ new_predictions = min_max_scaler.inverse_transform(new_predictions)
 
 last_date = new_data_df['date'].iloc[-1]
 next_day_date = last_date + pd.DateOffset(days=1)
-
+final_price = new_predictions[0, 0]
+percentage_change = ((final_price - initial_price) / initial_price) * 100
 print('Predicted Price for', next_day_date.date(), ':', new_predictions[0, 0])
+
+if percentage_change < 0:
+    print(f"Drop {abs(percentage_change):.2f}%.")
+elif percentage_change > 0:
+    print(f"Up {percentage_change:.2f}%.")
+else:
+    print("NDU")
+
+os.remove(TrainningData) if os.path.exists(TrainningData) else None
--- dataRetriever.py
+++ dataRetriever.py
@@ -2,36 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import pandas as pd
+import os
 
-url = "https://finance.yahoo.com/quote/TSLA/history?p=TSLA"
+def func():
+    TrainningData = "TrainningData.csv" 
+    os.remove(TrainningData) if os.path.exists(TrainningData) else None
+    url = "https://finance.yahoo.com/quote/GOOG/history?p=GOOG"
 
-column_headers = []
+    column_headers = []
 
-headers = {"User-Agent": UserAgent().random}
+    headers = {"User-Agent": UserAgent().random}
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "lxml")
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "lxml")
 
-table_tag = soup.find("table", class_="W(100%) M(0)")
-headers_tag = table_tag.find_all("th")
+    table_tag = soup.find("table", class_="W(100%) M(0)")
+    headers_tag = table_tag.find_all("th")
 
-for header_tag in headers_tag:
-    column_headers.append(header_tag.text)
+    for header_tag in headers_tag:
+        column_headers.append(header_tag.text)
 
-df = pd.DataFrame(columns=column_headers)
-data_rows = table_tag.find_all("tr")[1:101]
+    df = pd.DataFrame(columns=column_headers)
+    data_rows = table_tag.find_all("tr")[1:101]
 
-for row_tag in data_rows:
-    data_cells = row_tag.find_all("td")
-    row_data = [cell.text for cell in data_cells]
-    df_length = len(df)
-    df.loc[df_length] = row_data
+    for row_tag in data_rows:
+        data_cells = row_tag.find_all("td")
+        row_data = [cell.text for cell in data_cells]
+        df_length = len(df)
+        df.loc[df_length] = row_data
 
-df['Date'] = pd.to_datetime(df['Date'])
-df.set_index('Date', inplace=True)
-df["Volume"] = pd.to_numeric(df["Volume"].str.replace(",", ""), errors="coerce").astype('Int64')
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df["Volume"] = pd.to_numeric(df["Volume"].str.replace(",", ""), errors="coerce").astype('Int64')
 
-df[column_headers[1:]] = df[column_headers[1:]].apply(pd.to_numeric, errors='coerce')
-df.sort_index(inplace=True)
+    df[column_headers[1:]] = df[column_headers[1:]].apply(pd.to_numeric, errors='coerce')
+    df.sort_index(inplace=True)
 
-print(df)
+    df_rdc = df.reset_index()
+    df_rdc.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+    df_rdc.to_csv('TrainningData.csv', index=False)
+    return df_rdc["Close"][len(df_rdc)-1]
+
+func()
