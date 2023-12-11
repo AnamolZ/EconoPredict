0a4efc295680dd6f05d91064788a578ca1c5c3a9
AnamolZ <anmoldkl971@gmail.com>
2023-12-11 20:46:36 +0545

    Version 1.0

--- PulseAI.py
+++ PulseAI.py
@@ -5,33 +5,35 @@ import pandas as pd
 import os
 from dataRetriever import func
 
-loaded_model = load_model("LSTMmodel")
+def PulseAI(req):
+    loaded_model = load_model("LSTMmodel")
 
-initial_price = func()
-TrainningData = "TrainningData.csv" 
-new_data_df = pd.read_csv("TrainningData.csv", index_col=0)
-new_data_df['date'] = pd.to_datetime(new_data_df.index)
+    initial_price = func(req)
+    TrainningData = "TrainningData.csv" 
+    new_data_df = pd.read_csv("TrainningData.csv", index_col=0)
+    new_data_df['date'] = pd.to_datetime(new_data_df.index)
 
-min_max_scaler = MinMaxScaler(feature_range=(0, 1))
-min_max_scaler.fit_transform(new_data_df['Close'].values.reshape(-1, 1))
+    min_max_scaler = MinMaxScaler(feature_range=(0, 1))
+    min_max_scaler.fit_transform(new_data_df['Close'].values.reshape(-1, 1))
 
-new_dataset = min_max_scaler.transform(new_data_df['Close'].values.reshape(-1, 1))
-x_new = np.reshape(new_dataset[-15:], (1, 1, 15))
+    new_dataset = min_max_scaler.transform(new_data_df['Close'].values.reshape(-1, 1))
+    x_new = np.reshape(new_dataset[-15:], (1, 1, 15))
 
-new_predictions = loaded_model.predict(x_new)
-new_predictions = min_max_scaler.inverse_transform(new_predictions)
+    new_predictions = loaded_model.predict(x_new)
+    new_predictions = min_max_scaler.inverse_transform(new_predictions)
 
-last_date = new_data_df['date'].iloc[-1]
-next_day_date = last_date + pd.DateOffset(days=1)
-final_price = new_predictions[0, 0]
-percentage_change = ((final_price - initial_price) / initial_price) * 100
-print('Predicted Price for', next_day_date.date(), ':', new_predictions[0, 0])
+    last_date = new_data_df['date'].iloc[-1]
+    next_day_date = last_date + pd.DateOffset(days=1)
+    final_price = new_predictions[0, 0]
+    percentage_change = ((final_price - initial_price) / initial_price) * 100
 
-if percentage_change < 0:
-    print(f"Drop {abs(percentage_change):.2f}%.")
-elif percentage_change > 0:
-    print(f"Up {percentage_change:.2f}%.")
-else:
-    print("NDU")
+    if percentage_change < 0:
+        return (f'Price {new_predictions[0, 0]:.2f} ↟Up {abs(percentage_change):.2f}%')
 
-os.remove(TrainningData) if os.path.exists(TrainningData) else None
+    elif percentage_change > 0:
+        return (f'Price {new_predictions[0, 0]:.2f} ↯Down {abs(percentage_change):.2f}%')
+    else:
+        print("NDU")
+        pass
+
+    os.remove(TrainningData) if os.path.exists(TrainningData) else None   
--- dataRetriever.py
+++ dataRetriever.py
@@ -3,14 +3,19 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import pandas as pd
 import os
+import pandas as pd
+from datetime import datetime, timedelta
 
 # symbols = ['AMZN', 'GOOGL', 'TSLA', 'NFLX', 'META', 'AMZN', 'AMD', 'UBER']
 
-def func():
+def func(req):
+    current_datetime = datetime.now()
     TrainningData = "TrainningData.csv" 
+    MonthlyData = "MonthlyData.csv"
+    os.remove(MonthlyData) if os.path.exists(MonthlyData) else None
     os.remove(TrainningData) if os.path.exists(TrainningData) else None
     
-    url = "https://finance.yahoo.com/quote/NFLX/history?p=NFLX"
+    url = f"https://finance.yahoo.com/quote/{req}/history?p={req}"
 
     column_headers = []
 
@@ -44,4 +49,17 @@ def func():
     df_rdc = df.reset_index()
     df_rdc.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
     df_rdc.to_csv('TrainningData.csv', index=False)
+
+    file_path = "TrainningData.csv"
+    df = pd.read_csv(file_path)
+
+    one_month_ago = current_datetime - timedelta(days=41)
+    formatted_current_date = current_datetime.strftime("%Y-%m-%d")
+    formatted_one_month_ago = one_month_ago.strftime("%Y-%m-%d")
+
+    df["Date"] = pd.to_datetime(df["Date"])
+    filtered_dates = df[(df["Date"] >= formatted_one_month_ago) & (df["Date"] <= formatted_current_date)]
+    date = {"Date": filtered_dates["Date"].dt.strftime('%Y-%m-%d').tolist()}
+
+    filtered_dates.to_csv('MonthlyData.csv', index=False)
     return df_rdc["Close"][len(df_rdc)-1]
